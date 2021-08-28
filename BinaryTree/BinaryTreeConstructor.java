59758ee7043ac8870d566f75bfab8735b3fe9a0a
vishwajeet <singhvishwajeet1998@gmail.com>
2021-08-28 17:33:58 +0530

    Construction of Binary tree

--- BinaryTree/BinaryTreeConstructor.java
+++ BinaryTree/BinaryTreeConstructor.java
@@ -41,6 +41,37 @@ public class BinaryTreeConstructor {
 	public static void main(String args[]) {
 		Integer[] arr = {50, 25, 12, null, null, 37, 30, null, null, null, 75, 62, null, 70, null, null, 87, null, null};
 		
+//		Node root = new Node(arr[0], null, null);
+//		Pair rtp = new Pair(root, 1);
+//		Stack<Pair> stk = new Stack<>();
+//		stk.push(rtp);
+//		int idx = 0;
+//		while (stk.size() > 0) {
+//			Pair top = stk.peek();
+//			if (top.state == 1) {
+//				idx++;
+//				if (arr[idx] != null) {
+//					top.node.left = new Node(arr[idx], null, null);
+//					Pair lp = new Pair(top.node.left, 1);
+//					stk.push(lp);
+//				}
+//				else top.node.left = null;
+//				top.state++;
+//			}
+//			else if (top.state == 2) {
+//				idx++;
+//				if (arr[idx] != null) {
+//					top.node.right = new Node(arr[idx], null, null);
+//					Pair rp = new Pair(top.node.right, 1);
+//					stk.push(rp);
+//				}
+//				else top.node.right = null;
+//				top.state++;
+//			}
+//			else if (top.state == 3) stk.pop();
+//		}
+		
+		
 		Node root = new Node(arr[0], null, null);
 		Pair rtp = new Pair(root, 1);
 		Stack<Pair> stk = new Stack<>();
@@ -55,7 +86,9 @@ public class BinaryTreeConstructor {
 					Pair lp = new Pair(top.node.left, 1);
 					stk.push(lp);
 				}
-				else top.node.left = null;
+				else {
+					top.node.left = null;
+				}
 				top.state++;
 			}
 			else if (top.state == 2) {
@@ -70,6 +103,8 @@ public class BinaryTreeConstructor {
 			}
 			else if (top.state == 3) stk.pop();
 		}
+		
+		
 		display(root);
 	}
 }
